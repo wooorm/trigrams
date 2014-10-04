d6d21fc5da0473224a8c62f600e5cfa54e9eb84f
wooorm <tituswormer@gmail.com>
2014-10-04 22:04:20 +0200

    Add markdown-table as a dependency

--- build-supported-trigrams.js
+++ build-supported-trigrams.js
@@ -1,10 +1,14 @@
+'use strict';
+
 var fs,
+    table,
     udhr,
     allTrigrams,
     topTrigrams,
     minTrigrams;
 
 fs = require('fs');
+table = require('markdown-table');
 udhr = require('udhr').information();
 allTrigrams = require('./').all();
 topTrigrams = require('./').top();
@@ -18,51 +22,57 @@ fs.writeFileSync('Supported-trigrams.md',
     ' or [UDHR](https://github.com/wooorm/udhr/) for more information.\n' +
     '\n' +
 
-    '| Code | Name | Named version | OHCHR | All? | Top? | Min? |\n' +
-    '| ---: | ---- | ------------- | ----- | ---- | ---- | ---- |\n' +
+    table([
+            [
+                'Code', 'Name', 'Named version', 'OHCHR', 'All?',
+                'Top?', 'Min?'
+            ]
+        ].concat(Object.keys(udhr).map(function (code) {
+            var hasAll,
+                hasMin,
+                hasTop,
+                ohchr;
 
-    Object.keys(udhr).map(function (code) {
-        var hasAll,
-            hasMin,
-            hasTop,
-            ohchr;
+            if (code in allTrigrams) {
+                hasAll = '[Yes](data/all/' + code + '.json)';
+            } else {
+                hasAll = 'No';
+            }
 
-        if (code in allTrigrams) {
-            hasAll = '[Yes](data/all/' + code + '.json)';
-        } else {
-            hasAll = 'No';
+            if (code in minTrigrams) {
+                hasMin = '[Yes](data/min/' + code + '.json)';
+            } else {
+                hasMin = 'No';
+            }
+
+            if (code in topTrigrams) {
+                hasTop = '[Yes](data/top/' + code + '.json)';
+            } else {
+                hasTop = 'No';
+            }
+
+            if (udhr[code].OHCHR) {
+                ohchr = '[' + udhr[code].OHCHR + ']' +
+                    '(http://www.ohchr.org/EN/UDHR/Pages/Language.aspx?' +
+                    'LangID=' + udhr[code].OHCHR + ')';
+            } else {
+                ohchr = '';
+            }
+
+            return [
+                code,
+                udhr[code].name,
+                (udhr[code].namedVersion || ''),
+                ohchr,
+                hasAll,
+                hasTop,
+                hasMin
+            ];
+        })),
+        {
+            'align' : ['r']
         }
-
-        if (code in minTrigrams) {
-            hasMin = '[Yes](data/min/' + code + '.json)';
-        } else {
-            hasMin = 'No';
-        }
-
-        if (code in topTrigrams) {
-            hasTop = '[Yes](data/top/' + code + '.json)';
-        } else {
-            hasTop = 'No';
-        }
-
-        if (udhr[code].OHCHR) {
-            ohchr = '[' + udhr[code].OHCHR + ']' +
-                '(http://www.ohchr.org/EN/UDHR/Pages/Language.aspx?LangID=' +
-                udhr[code].OHCHR + ')';
-        } else {
-            ohchr = '';
-        }
-
-        return '| ' +
-            code +
-            ' | ' + udhr[code].name +
-            ' | ' + (udhr[code].namedVersion || '') +
-            ' | ' + ohchr +
-            ' | ' + hasAll +
-            ' | ' + hasTop +
-            ' | ' + hasMin +
-            ' |';
-    }).join('\n') +
+    ) +
 
     '\n'
 );
