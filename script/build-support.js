85594c8afc36c623ea15f8f375772ea7358a7481
wooorm <tituswormer@gmail.com>
2014-11-01 12:24:30 +0100

    Refactor `script/build-support.js`

--- script/build-support.js
+++ script/build-support.js
@@ -1,18 +1,35 @@
 'use strict';
 
+/**
+ * Dependencies.
+ */
+
 var fs,
     table,
-    udhr,
+    udhr;
+
+fs = require('fs');
+table = require('markdown-table');
+udhr = require('udhr');
+
+/**
+ * Data.
+ */
+
+var information,
     allTrigrams,
     topTrigrams,
     minTrigrams;
 
-fs = require('fs');
-table = require('markdown-table');
-udhr = require('udhr').information();
+information = udhr.information();
+
 allTrigrams = require('../').all();
 topTrigrams = require('../').top();
 minTrigrams = require('../').min();
+
+/**
+ * Write support information.
+ */
 
 fs.writeFileSync('Supported-trigrams.md',
     'Supported Trigrams\n' +
@@ -27,7 +44,7 @@ fs.writeFileSync('Supported-trigrams.md',
                 'Code', 'Name', 'Named version', 'OHCHR', 'All?',
                 'Top?', 'Min?'
             ]
-        ].concat(Object.keys(udhr).map(function (code) {
+        ].concat(Object.keys(information).map(function (code) {
             var hasAll,
                 hasMin,
                 hasTop,
@@ -51,18 +68,18 @@ fs.writeFileSync('Supported-trigrams.md',
                 hasTop = 'No';
             }
 
-            if (udhr[code].OHCHR) {
-                ohchr = '[' + udhr[code].OHCHR + ']' +
+            if (information[code].OHCHR) {
+                ohchr = '[' + information[code].OHCHR + ']' +
                     '(http://www.ohchr.org/EN/UDHR/Pages/Language.aspx?' +
-                    'LangID=' + udhr[code].OHCHR + ')';
+                    'LangID=' + information[code].OHCHR + ')';
             } else {
                 ohchr = '';
             }
 
             return [
                 code,
-                udhr[code].name,
-                (udhr[code].namedVersion || ''),
+                information[code].name,
+                (information[code].namedVersion || ''),
                 ohchr,
                 hasAll,
                 hasTop,
