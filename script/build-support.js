d60accb863d2b345ce0658312aab660dc3a7ecea
Titus Wormer <tituswormer@gmail.com>
2022-11-22 10:58:04 +0100

    Add strict types

--- script/build-support.js
+++ script/build-support.js
@@ -11,70 +11,63 @@ import {u} from 'unist-builder'
 import {udhr} from 'udhr'
 import {min} from '../index.js'
 
-export default function support() {
-  return transformer
-}
-
 /**
- * @param {Root} tree
+ * @type {import('unified').Plugin<[], Root, Root>}
  */
-async function transformer(tree) {
-  const data = await min()
+export default function support() {
+  return async function (tree) {
+    const data = await min()
 
-  zone(tree, 'support', replace)
-
-  /**
-   * @param {Node?} start
-   * @param {unknown} _
-   * @param {Node?} end
-   */
-  function replace(start, _, end) {
-    return [start, table(), end]
-  }
-
-  /**
-   * @returns {Table}
-   */
-  function table() {
-    const content = [u('tableRow', [cell('Code'), cell('Name'), cell('OHCHR')])]
-    let index = -1
-
-    while (++index < udhr.length) {
-      if (!(udhr[index].code in data)) {
-        continue
-      }
-
-      content.push(
-        u('tableRow', [
-          cell(udhr[index].code),
-          cell(udhr[index].name),
-          cell(
-            udhr[index].ohchr
-              ? u(
-                  'link',
-                  {
-                    url:
-                      'https://www.ohchr.org/EN/UDHR/Pages/Language.aspx?LangID=' +
-                      udhr[index].ohchr
-                  },
-                  [u('text', udhr[index].ohchr)]
-                )
-              : 'No'
-          )
-        ])
-      )
-    }
-
-    return u('table', {align: []}, content)
+    zone(tree, 'support', function (start, _, end) {
+      return [start, table(), end]
+    })
 
     /**
-     * @param {string|PhrasingContent} value
-     * @returns {TableCell}
+     * @returns {Table}
      */
-    function cell(value) {
-      return u('tableCell', [
-        typeof value === 'string' ? u('text', value) : value
-      ])
+    function table() {
+      const content = [
+        u('tableRow', [cell('Code'), cell('Name'), cell('OHCHR')])
+      ]
+      let index = -1
+
+      while (++index < udhr.length) {
+        if (!(udhr[index].code in data)) {
+          continue
+        }
+
+        content.push(
+          u('tableRow', [
+            cell(udhr[index].code),
+            cell(udhr[index].name),
+            cell(
+              udhr[index].ohchr
+                ? u(
+                    'link',
+                    {
+                      url:
+                        'https://www.ohchr.org/EN/UDHR/Pages/Language.aspx?LangID=' +
+                        udhr[index].ohchr
+                    },
+                    [u('text', udhr[index].ohchr)]
+                  )
+                : 'No'
+            )
+          ])
+        )
+      }
+
+      return u('table', {align: []}, content)
+
+      /**
+       * @param {string|PhrasingContent} value
+       * @returns {TableCell}
+       */
+      function cell(value) {
+        return u('tableCell', [
+          typeof value === 'string' ? u('text', value) : value
+        ])
+      }
     }
   }
 }
