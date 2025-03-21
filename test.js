b9fe4050b7df3c0e2f304b6069a3efef35311f57
Titus Wormer <tituswormer@gmail.com>
2025-03-21 12:35:45 +0100

    Change to use `exports`

--- test.js
+++ test.js
@@ -1,10 +1,10 @@
 import assert from 'node:assert/strict'
 import test from 'node:test'
-import {min, top} from './index.js'
+import {min, top} from 'trigrams'
 
 test('trigrams', async function (t) {
   await t.test('should expose the public api', async function () {
-    assert.deepEqual(Object.keys(await import('./index.js')).sort(), [
+    assert.deepEqual(Object.keys(await import('trigrams')).sort(), [
       'min',
       'top'
     ])
